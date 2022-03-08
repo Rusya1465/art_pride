f45a2f8f2af077c452ca7b3de0d0320f46c34879
Rustam <rustamotorbaev@gmail.com>
2022-03-08 22:16:27 +0600

    catalog-main end

--- src/components/Catalog/CatalogMain/Description.js
+++ src/components/Catalog/CatalogMain/Description.js
@@ -1,14 +1,7 @@
 const Description = ({ text, img }) => {
   return (
     <>
-      <div className="article">
-        <div className="article_text">
-          <p>{text}</p>
-        </div>
-        <div className="article_img">
-          <img src={img} />
-        </div>
-      </div>
+      
     </>
   );
 };
---
+++ src/components/Catalog/CatalogMain/DetailBtn.js
@@ -0,0 +1,11 @@
+import Arrow from "./CatalogImg/Arrow.svg";
+const DetailBtn = () => {
+  return (
+    <div className="detail_btn">
+      <p>Подробнее</p>
+      <img src={Arrow} />
+    </div>
+  );
+};
+
+export default DetailBtn;
